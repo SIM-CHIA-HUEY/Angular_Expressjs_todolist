133fbab37219d0422a5b56ffd96e8d827c9f351a
Chia <chia-huey.sim@deepomatic.com>
2022-06-20 16:57:32 +0200

    Modifying tab and buttons experience

--- frontend/src/app/right/tasklist/tasklist.component.ts
+++ frontend/src/app/right/tasklist/tasklist.component.ts
@@ -7,16 +7,20 @@ import axios from "axios";
   styleUrls: ['./tasklist.component.scss']
 })
 export class TasklistComponent implements OnInit {
-
-  id: any = "all";
+  // id: string = "all";
+  tab = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.tab == 0) {
+      this.tab = 1;
+    }
+    // this.tabChange("todo");
   }
 
-  tabChange(ids: string) {
-    console.log(ids);
-      this.id = ids;
-  }
+  // tabChange(ids: string) {
+  //   // console.log(ids);
+  //     this.id = ids;
+  // }
 }
