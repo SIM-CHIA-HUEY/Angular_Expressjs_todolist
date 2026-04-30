65a6fd136aa3ace2156e36267e2008ebfe3fe26c
SIM Chia Huey <sim.chiahuey@gmail.com>
2026-04-30 18:19:55 +0200

    backup before angular upgrade

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +15,8 @@ import { StoreModule } from '@ngrx/store';
 import { TodosComponent } from './todos/todos.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { counterReducer } from '../app/store/reducer/counter.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,10 @@ import { counterReducer } from '../app/store/reducer/counter.reducer';
       }),
       StoreModule.forRoot({
         counterReducer
-    }), // + effect !
+    }),
+      StoreModule.forRoot({}, {}),
+      EffectsModule.forRoot([]),
+      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }), // + effect !
   ],
   providers: [],
   // declarations: [AppComponent],
