6fb83963afdfdd84a0cd1d310c9941a258acadb0
SIM Chia Huey <sim.chiahuey@gmail.com>
2026-08-11 14:38:57 +0200

    Solved cors problem on dev and build mode

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-//   origin: 'http://localhost:4200'
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:4200', 'http://localhost:3000']
 }));
 
 app.use("/todos", todosRouter);
